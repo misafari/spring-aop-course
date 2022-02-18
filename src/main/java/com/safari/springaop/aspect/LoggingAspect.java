d699e9adb11c18af4617837453678d49e84f8b33
me.safari <ms.safari@outlook.com>
2022-02-18 13:05:07 -0800

    after throwing

--- src/main/java/com/safari/springaop/aspect/LoggingAspect.java
+++ src/main/java/com/safari/springaop/aspect/LoggingAspect.java
@@ -1,15 +1,18 @@
 package com.safari.springaop.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Before("execution(public void save())")
-    public void logInfo() {
+    @AfterThrowing(pointcut = "execution(public void save(boolean))", throwing = "ex")
+    public void logInfo(JoinPoint joinPoint, Throwable ex) {
         System.out.println(getClass() + "Logging Ino <=======================");
+        System.out.println(ex.getMessage());
     }
+
 }
--- src/main/java/com/safari/springaop/service/UserService.java
+++ src/main/java/com/safari/springaop/service/UserService.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public void save() {
+
+    public void save(boolean withErr) throws Exception {
         System.out.println("UserService Save Done.");
+        if (withErr) {
+            throw new Exception("Custom Error");
+        }
     }
 }
